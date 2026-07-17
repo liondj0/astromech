873c7bd171c59bdf5faf9c695fb55baa34160765
lavdjuric <liondj007@gmail.com>
2026-07-18 00:24:57 +0200

    Testing motor

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	hardware2 "astromech/hardware"
 	"astromech/hardware/motor"
-	voice2 "astromech/voice"
 	"time"
 )
 
 func main() {
 	hardware := hardware2.InitHardware()
-	voice := voice2.NewVoice(hardware.Speaker)
-	voice.Speak(`Hello there!`)
-	time.Sleep(1 * time.Second)
-	voice.Speak(`General Kenobi!`)
-	time.Sleep(1 * time.Second)
+	//voice := voice2.NewVoice(hardware.Speaker)
+	//voice.Speak(`Hello there!`)
+	//time.Sleep(1 * time.Second)
+	//voice.Speak(`General Kenobi!`)
+	//time.Sleep(1 * time.Second)
 	println("Turning head right")
 	hardware.HeadMotor.TurnSeconds(100, motor.Right)
 	hardware.HeadMotor.TurnDegrees(60, motor.Right)
